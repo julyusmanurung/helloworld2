81de9aa5d4bdc2a2d30c41342a6355b10e9843b8
julyusmanurung <julyusmanurung@gmail.com>
2022-09-28 14:52:54 +0700

    adding POST method and text input for frontend

--- backend/main.go
+++ backend/main.go
@@ -11,16 +11,34 @@ import (
 
 func handler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"message": "hello world",
+		"data": db,
 	})
 }
 
+var db []string
+
+type DataRequest struct {
+	Text string `json:"text"`
+}
+
+func postHandler(c *gin.Context) {
+	var data DataRequest
+	if err := c.ShouldBindJSON(&data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db = append(db, data.Text)
+	c.JSON(http.StatusOK, gin.H{"message": "data berhasil terkirim", "data": data.Text})
+}
+
 func main() {
+	db = make([]string, 0)
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"*"},
 	}))
 
 	r.GET("/", handler)
+	r.POST("/send", postHandler)
 	r.Run(":" + os.Getenv("PORT"))
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,9 +7,10 @@ export default function Home() {
     getData()
   }, []);
 
+  console.log(data?.data)
   const getData = async () => {
     try {
-      const res = await fetch("https://helloworld2-frontend-i24x.vercel.app/");
+      const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL);
       const data = await res.json();
       setData(data);
     }
@@ -18,17 +19,56 @@ export default function Home() {
     }
   }
 
-  if (error) {
-    return <div>Failed to load {error.toString()}</div>
-  }
-
-  if (!data) {
-    return <div>Loading...</div>
-  }
-
   return (
     <div>
-      <p>data: {data.message}</p>
+      {error && <div>Failed to load {error.toString()}</div>}
+      {
+        !data ? <div>Loading...</div>
+          : (
+            (data?.data ?? []).length === 0 && <p>data kosong</p>
+          )
+      }
+
+      <Input onSuccess={getData} />
+      {data?.data && data?.data?.map((item, index) => (
+        <p key={index}>{item}</p>
+      ))}
     </div>
   )
+
+  function Input({onSuccess}) {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const formData = new FormData(e.currentTarget);
+      const body = {
+        text: formData.get("data")
+      }
+  
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/send`, {
+          method: 'POST',
+          body: JSON.stringify(body)
+        });
+        const data = await res.json();
+        setData(data.message);
+        onSuccess();
+      }
+      catch (error) {
+        setError(error);
+      }
+    }
+    return (
+      <div>
+        {error && <p>error: {error.toString()}</p>}
+        {data && <p>success: {data}</p>}
+        <form onSubmit={handleSubmit}>
+          <input name="data" type="text" />
+          <button >Submit</button>
+        </form>
+      </div>
+    )
+  }
 }
