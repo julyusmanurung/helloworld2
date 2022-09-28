86ddbed3f8b9e6ed4fbdc469d47f650d0a2098d7
julyusmanurung <julyusmanurung@gmail.com>
2022-09-28 23:02:37 +0700

    test

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -68,7 +68,7 @@ export default function Home() {
               <label className="inline-block w-full text-xl font-medium text-gray-500 transition-all duration-200 ease-in-out group-focus-within:text-blue-400">Masukkan sesuatu disini...</label>
               <div className="relative flex items-center">
                 <input type="text" className="peer relative h-10 w-full rounded-md bg-gray-50 pl-4 pr-20 font-thin outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:drop-shadow-lg" name="data"/>
-                <button className="absolute right-2 h-7 w-16 rounded-md bg-blue-200 text-sm font-semibold text-white transition-all duration-200 ease-in-out group-focus-within:bg-blue-400 group-focus-within:hover:bg-blue-600">Submit</button>
+                <button className="absolute right-2 h-7 w-16 rounded-md bg-blue-200 text-sm font-semibold text-white transition-all duration-200 ease-in-out group-focus-within:bg-blue-400 group-focus-within:hover:bg-blue-600">Simpan</button>
               </div>
             </div>
           </form>
